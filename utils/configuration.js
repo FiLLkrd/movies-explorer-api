38ee7f7c11e2be45db57919c6019e00dd901a2f6
FiLLkrd <ma.antonoff2012@yandex.ru>
2023-07-12 23:59:49 +0300

    меняет порт

--- utils/configuration.js
+++ utils/configuration.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
-const { PORT = 3001 } = process.env;
-const { DB_URL = 'mongodb://127.0.0.1/bitfilmsdb' } = process.env;
+const { PORT = 3000 } = process.env;
+const { DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const { NODE_ENV } = process.env;
 const { JWT_SECRET_PROD } = process.env;
 const JWT_SECRET = NODE_ENV === 'production' ? JWT_SECRET_PROD : 'dev-secret';
