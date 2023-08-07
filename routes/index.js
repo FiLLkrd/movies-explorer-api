dd2bea5f67a2d447f8375b89a045701f08b6e418
FiLLkrd <ma.antonoff2012@yandex.ru>
2023-08-07 20:48:48 +0300

    исправление бага с авторизацией

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
+const { createUser, login } = require('../controllers/users');
+const { auth } = require('../middlewares/auth');
 const userRouter = require('./users');
 const moviesRouter = require('./movies');
-const { createUser, login } = require('../controllers/users');
-
 const ErrNotFound = require('../utils/ErrNotFound');
 const { signUpValidate, signInValidate } = require('../middlewares/validation');
 
 router.post('/signup', signUpValidate, createUser);
 router.post('/signin', signInValidate, login);
-
+router.use(auth);
 router.use('/users', userRouter);
 router.use('/movies', moviesRouter);
 router.use('*', (req, res, next) => {
--- routes/movies.js
+++ routes/movies.js
@@ -1,15 +1,13 @@
 const router = require('express').Router();
 const { deleteMovieValidation, postMovieValidation } = require('../middlewares/validation');
-const { auth } = require('../middlewares/auth');
 const {
   getMovies,
   createMovies,
   deleteMovies,
 } = require('../controllers/movies');
 
-router.use(auth);
 router.get('/', getMovies);
 router.post('/', postMovieValidation, createMovies);
-router.delete('/', deleteMovieValidation, deleteMovies);
+router.delete('/:movieId', deleteMovieValidation, deleteMovies);
 
 module.exports = router;
