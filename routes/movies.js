68c09a77ad06c98b54cab61e820322c6d6cb300a
FiLLkrd <ma.antonoff2012@yandex.ru>
2023-08-07 20:29:48 +0300

    исправление бага с авторизацией

--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const { deleteMovieValidation, postMovieValidation } = require('../middlewares/validation');
+const { auth } = require('../middlewares/auth');
 const {
   getMovies,
   createMovies,
   deleteMovies,
 } = require('../controllers/movies');
 
+router.use(auth);
 router.get('/', getMovies);
 router.post('/', postMovieValidation, createMovies);
 router.delete('/', deleteMovieValidation, deleteMovies);
