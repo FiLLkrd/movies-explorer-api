323daf393f6fea5098d025adb756734f8734d06f
FiLLkrd <ma.antonoff2012@yandex.ru>
2023-08-10 00:23:10 +0300

    изменяет статусы ошибок

--- utils/errors.js
+++ utils/errors.js
@@ -1,13 +1,11 @@
-const http2 = require('http2');
-
-const NOT_FOUND = http2.constants.HTTP_STATUS_NOT_FOUND;
-const INTERNAL_SERVER_ERROR = http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
-const BAD_REQUEST = http2.constants.HTTP_STATUS_BAD_REQUEST;
-const OK = http2.constants.HTTP_STATUS_OK;
-const CREATED = http2.constants.HTTP_STATUS_CREATED;
-const CONFLICT = http2.constants.HTTP_STATUS_CONFLICT;
-const UNAUTH_ERR = http2.constants.HTTP_STATUS_UNAUTHORIZED;
-const FORBIDDEN = http2.constants.HTTP_STATUS_FORBIDDEN;
+const NOT_FOUND = 404;
+const INTERNAL_SERVER_ERROR = 500;
+const BAD_REQUEST = 400;
+const OK = 200;
+const CREATED = 201;
+const CONFLICT = 409;
+const UNAUTH_ERR = 401;
+const FORBIDDEN = 403;
 
 module.exports = {
   NOT_FOUND,
