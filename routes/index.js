77cd2701c710a9dd83ffdf2c815a9550914407e7
FiLLkrd <ma.antonoff2012@yandex.ru>
2023-08-11 23:31:04 +0300

    обновил бэк из-за ошибок авторизации

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 const router = require('express').Router();
 const { createUser } = require('../controllers/users');
 const { login } = require('../controllers/login');
-const ErrNotFound = require('../utils/ErrNotFound');
 const { signUpValidate, signInValidate } = require('../middlewares/validation');
 
 router.post('/signup', signUpValidate, createUser);
 router.post('/signin', signInValidate, login);
-router.use('*', (req, res, next) => {
-  next(new ErrNotFound('Страница не найдена'));
-});
 
 module.exports = router;
